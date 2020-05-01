830599ceebfbfe3e10557be0ca03a98bf1478de2
Val-E <30366552+Val-E@users.noreply.github.com>
2020-05-01 13:43:03 +0200

    Update row.js

--- vocabulary game/rsc/js/row.js
+++ vocabulary game/rsc/js/row.js
@@ -1,212 +1,350 @@
 
-
-/*
- Each q(number)_(number) is a question. The first number is the column (category), the secound is the level. 
- How you add/change question:
- q1_1 = [{'q': 'Your first Question.', 'a': 'the answer', 'p': 'the points for this question' }, ... {'q': '...', 'a': '...', p: '...' }, {'q': 'end'}]
- 
- This object is at the end of each list and is not a question, but a necessary flag for the script. Make sure that you did not deleted it! 
- ,{'q': 'end'}
- 
- This object is at the start of each list, it is used for counting. Make sure for that you do not delet it will editing.
- {'c': 0},
+challenges = [
+/* You can add here your questions. 
+   Form: {'q':"Your Question", 'a': 'Your answer'},
+   The questions will be asked in the ordere in which they are listed here.
+   If you play without skipping make sure to have at least 42 questions/challenges/etc. !
+   If you play more add as much as possible.
  */
-q1_1 = [{'c': 1}, {'q': 'abhaken', 'a': 'tick', 'p': '1'}, {'q': 'einzigartig', 'a': 'unique', 'p': '1'}, {'q': 'Wahl', 'a': 'election', 'p': '1'}, {'q': 'end'}];
-q1_2 = [{'c': 1}, {'q': 'Willenskraft', 'a': 'willpower', 'p': '2'}, {'q': 'Streben', 'a': 'persuit', 'p': '2'}, {'q': 'Melancholie', 'a': 'melancholy', 'p': '2'},{'q': 'end'}];
-q1_3 = [{'c': 1}, {'q': 'bedrohlich', 'a': 'threating', 'p': '3'}, {'q': 'denkw&uuml;rdig', 'a': 'memorable', 'p': '3'}, {'q': 'alt, antik', 'a': 'ancient', 'p': '3'},{'q': 'end'}];
-q1_4 = [{'c': 1}, {'q': 'Schmelztiegel', 'a': 'melting pot', 'p': '4'}, {'q': 'atemberaubend', 'a': 'breathtaking', 'p': '4'}, {'q': 'geschieden', 'a': 'divorced', 'p': '4'},{'q': 'end'}];
-q2_1 = [{'c': 1}, {'q': 'At the end of a ... is a pot of gold.', 'a': 'rainbow', 'p': '1'}, {'q': 'We need a ... to our problem.', 'a': 'solution', 'p': '1'}, {'q': 'end'}];
-q2_2 = [{'c': 1}, {'q': 'She used a ... from a speech for her essay.', 'a': 'quote', 'p': '2'}, {'q': 'A lot of English words have Latin ... .', 'a': 'origins', 'p': '2'}, {'q': 'end'}];
-q2_3 = [{'c': 1}, {'q': 'The sky is ... us.', 'a': 'above', 'p': '3'}, {'q': 'The fall of the Berlin Wall was a major ... in German history.', 'a': 'event', 'p': '3'}, {'q': 'end'}];
-q2_4 = [{'c': 1}, {'q': 'He needed a ... to solve the riddle.', 'a': 'hint', 'p': '4'}, {'q': 'Horror films use ... music to scare the audience. Note: "Scary" IS NOT the solution!', 'a': 'sinister', 'p': '4'}, {'q': 'end'}];
-//If you wanne change the audio, you have to edit the path: src='...' or you go replace the mp3 file with your file!
-q3_1 = [{'c': 1}, {'q': "<audio controls><source src='../vocabulary game/rsc/misc/q3_1.mp3'></audion", 'a': 'introduce', 'p': 1}, {'q': 'end'}];
-q3_2 = [{'c': 1}, {'q': "<audio controls><source src='../vocabulary game/rsc/misc/q3_2.mp3'></audion", 'a': 'exceptional', 'p': 2}, {'q': 'end'}];
-q3_3 = [{'c': 1}, {'q': "<audio controls><source src='../vocabulary game/rsc/misc/q3_3.mp3'></audion", 'a': 'leisure', 'p': 3}, {'q': 'end'}];
-q3_4 = [{'c': 1}, {'q': "<audio controls><source src='../vocabulary game/rsc/misc/q3_4.mp3'></audion", 'a': 'contemporary', 'p': 4}, {'q': 'end'}];
+{'q':'Regenbogen', 'a':'rainbow'}, 
+{'q':'visuell', 'a':'visual'},
+{'q':'wie (zum Beispiel)', 'a':'such as'}, 
+{'q':'&Uuml;berw&auml;ltigend', 'a':'overwhelming'},
+{'q':'Mehrheit', 'a':'majority'}, 
+{'q':"zu jdm/etw passen (Note: Just the word, without 'jdm/etw' or 'to')", 'a':'suit'},
+{'q':'alt, antik', 'a': 'ancient'}, 
+{'q':'Landschaft (Note: landscape is not meant)', 'a':'scenery'},
+{'q':'jenenseits (von), &uuml;ber', 'a':'beyond'}, 
+{'q':'unvergesslich', 'a':'unforgettable'},
+{'q':'Schmelztiegel', 'a':'melting pot'}, 
+{'q':'Kreativit&auml;t', 'a':'creativity'},
+{'q':'atemberaubend', 'a':'breathtaking'}, 
+{'q':'Disziplin', 'a':'discipline'},
+{'q':'Welle', 'a':'wave'}, 
+{'q':'Freizeit', 'a':'leisure'},
+{'q':'umgebend', 'a':'surrounding'}, 
+{'q':'Streben', 'a':'pursuit'},
+{'q':'großartig, fantastisch, toll', 'a':'terrific'}, 
+{'q':'einzigartig', 'a':'unique'},
+{'q':'abhaken', 'a':'tick'}, 
+{'q':'im Gegensatz zu', 'a':'in contrast to'},
+{'q':'(be)f&ouml;rdern, aufsteigen; hier: werben für', 'a':'promote'}, 
+{'q':'Apartheid', 'a':'apartheid'},
+{'q':'aushängen, an(zeigen)', 'a':'display'}, 
+{'q':'vorstellen; hier: einführen', 'a':'introduce'},
+{'q':'Ziel, Tor', 'a':'goal'}, 
+{'q':'vereinigen, verbinden', 'a':'unite'},
+{'q':'deshalb, deswegen, daher', 'a':'therefore'}, 
+{'q':'erf&uuml;llen, verwirklichen', 'a':'fulfil'},
+{'q':'(sich) bewegend', 'a':'moving'}, 
+{'q':'akkurat', 'a':'accurate'},
+{'q':'authentisch', 'a':'authentic'}, 
+{'q':'Einkommen', 'a':'income'},
+{'q':'sch&auml;tzen, zu schätzen wissen', 'a':'appreciate'}, 
+{'q':'beruhigend', 'a':'calming'},
+{'q':'Zitat', 'a':'quote'}, 
+{'q':'oberhalb', 'a':'above'},
+{'q':'hoffnungslos', 'a':'hopeless'}, 
+{'q':'Bilderrahmen', 'a':'frame'},
+{'q':'Ursprung, Herkunft', 'a':'origin'}, 
+{'q':'Prozentsatz', 'a':'percentage'},
+{'q':'If you see this, you have a draw, you skipped to much questions and now we are runned out of them or you broke the program.', 'a':''}
+]
 
-
-//the main html-code for the quiz game
-quiz_game = 
-"<table border='5px'>" +
-   "<tr>"+
-      "<th>Level</th><th>Translate</th><th>Guess</th><th>Spell</th>" +
-   "</tr>" +
-   "<tr>"  +
-      "<td><center>1</center></td>"+
-      "<td><input type='button' value='Question 1' onclick='getquestion(q1_1)'/></td>"+
-      "<td><input type='button' value='Question 1' onclick='getquestion(q2_1)'/></td>" +
-      "<td><input type='button' value='Listen 1' onclick='getquestion(q3_1)'/></td>" +
-   "<tr>" +
-      "<td><center>2</center></td>" +
-      "<td><input type='button' value='Question 2' onclick='getquestion(q1_2)'/></td>" +
-      "<td><input type='button' value='Question 2' onclick='getquestion(q2_2)'/></td>" +
-      "<td><input type='button' value='Listen 2' onclick='getquestion(q3_2)'/></td>" +
-   "</tr>"+
-   "<tr>" +
-      "<td><center>3</center></td>"+
-      "<td><input type='button' value='Question 3' onclick='getquestion(q1_3)'/></td>"+
-      "<td><input type='button' value='Question 3' onclick='getquestion(q2_3)'/></td>"+
-      "<td><input type='button' value='Listen 3' onclick='getquestion(q3_3)'/></td>"+
-   "</tr>"+
-   "<tr>" +
-      "<td><center>4</center></td>"+
-      "<td><input type='button' value='Question 4' onclick='getquestion(q1_4)'/></td>"+
-      "<td><input type='button' value='Question 4' onclick='getquestion(q2_4)'/></td>"+
-      "<td><input type='button' value='Listen 4' onclick='getquestion(q3_4)'/></td>"+
-   "</tr>"+
+//html-code for the game
+row_game=
+"<table>"+
+"<tr>" +
+   "<td id='r00' onclick='createchallenge(0)'> </td>" +
+   "<td id='r01' onclick='createchallenge(1)'> </td>" +
+   "<td id='r02' onclick='createchallenge(2)'> </td>" +
+   "<td id='r03' onclick='createchallenge(3)'> </td>" +
+   "<td id='r04' onclick='createchallenge(4)'> </td>" +
+   "<td id='r05' onclick='createchallenge(5)'> </td>" +
+   "<td id='r06' onclick='createchallenge(6)'> </td>" +
+"</tr>" +
+"<tr>" +
+   "<td id='r10'> </td>" +
+   "<td id='r11'> </td>" +
+   "<td id='r12'> </td>" +
+   "<td id='r13'> </td>" +
+   "<td id='r14'> </td>" +
+   "<td id='r15'> </td>" +
+   "<td id='r16'> </td>" +
+"</tr>" +
+"<tr>" +
+   "<td id='r20'> </td>" +
+   "<td id='r21'> </td>" +
+   "<td id='r22'> </td>" +
+   "<td id='r23'> </td>" +
+   "<td id='r24'> </td>" +
+   "<td id='r25'> </td>" +
+   "<td id='r26'> </td>" +
+"</tr>" +
+"<tr>" +
+   "<td id='r30'> </td>" +
+   "<td id='r31'> </td>" +
+   "<td id='r32'> </td>" +
+   "<td id='r33'> </td>" +
+   "<td id='r34'> </td>" +
+   "<td id='r35'> </td>" +
+   "<td id='r36'> </td>" +
+"</tr>" +
+"<tr>" +
+   "<td id='r40'> </td>" +
+   "<td id='r41'> </td>" +
+   "<td id='r42'> </td>" +
+   "<td id='r43'> </td>" +
+   "<td id='r44'> </td>" +
+   "<td id='r45'> </td>" +
+   "<td id='r46'> </td>" +
+"</tr>" +
+"<tr>" +
+   "<td id='r50'> </td>" +
+   "<td id='r51'> </td>" +
+   "<td id='r52'> </td>" +
+   "<td id='r53'> </td>" +
+   "<td id='r54'> </td>" +
+   "<td id='r55'> </td>" +
+   "<td id='r56'> </td>" +
+"</tr>" +
 "</table>"+
-"<hr>";
+"<link rel='stylesheet' type='text/css' href='./rsc/css/row.css'>"
 
-//the html code for the submit area
-submit = "<ul><p>Question: <nav id='question'></nav></p><p><input id='answer' type='text'/><input type='submit' value='answer' onclick='check()'/></p></ul>"
-noquestions = "The questions for this level are runned out. Try a another level or a another category!"
+//counts which questions are used
+counter = 0;
 
 //saves the question
-question = "";
+cha = "";
 
-//saves the correct answer
-answer = "take";
+//saves the answer
+ans = "";
 
-//saves the user's answer
-useranswer = "";
+//saves the id of the question
+quest_id = 0;
 
-//save the points
-points = 0;
+//keeps track of players
+cur = 1;
+s1 = "red";
+s2 = "yellow";
 
-//array archives the points of th and make them assignable
-teams = [];
+//creating an array for keeping track of the balls
+arr = [];
+for(var a=0;a<6;a++) {
+   arr[a] = [0,0,0,0,0,0,0];
+}
 
-//variables to track rounds and teams
-team = 0;
-team_count = 0;
-round = 0;
-round_count = 0;
-teams_length = 0;
-team_length=0;
-i = 0;
-
-//main function 
-function quiz() {   
-   team = Math.floor(Math.abs(document.getElementById('teams').value));
-   round = Math.floor(Math.abs(document.getElementById('rounds').value));
-   round_count = team*round;
-   teams_length = team;
-   team_length = team;
-   
-   //Error messages, if the players forget to say how many teams they are and how many rounds they want to play.
-   if(team == 0 || team == '') {
-      alert('Need at least one player/team.');
-      return;
-   }
-   if(round == 0 || round == '') {
-         alert('Need at least one round.');
-         return;
-   }
-   
-   //adds elements to the array, with the value of 0
-   //the value are the points the player gets while playing
-   while(team != 0) {
-      teams.unshift(0);
-      team--;
-   }
-   
-   //creates the game 
-   document.getElementById('main').innerHTML = quiz_game;
+function row() {
+   document.getElementById('main').innerHTML = row_game;
    document.getElementById('addteams').innerHTML = "";
    document.getElementById('addrounds').innerHTML = "";
-}   
-
-
-//creates the submit area
-function setquestion(question) {
-   document.getElementById('main').innerHTML = submit;
-   document.getElementById('question').innerHTML = question;
+}
+function challenge() {
+   var answer = document.getElementById('answer').value;
+   document.getElementById('submitter1').innerHTML= "";
+   document.getElementById('submitter2').innerHTML= "";
+   if (answer == ans) { move(quest_id); }
+   else if (answer != ans) { alert('Your answer: ' + answer + '; Correct answer: ' + ans); }
 }
 
-//function for setting up the questions 
-function getquestion(q_id) {
-   //setting up z to the name of the object
-   //so I can call z instead of checking each objects name
-   var z = q_id;
+//function for creating the submit area and setting up the question
+function createchallenge(id) {
+   quest_id = id;
+   cha = challenges[counter].q;
+   ans = challenges[counter].a;
+   counter++;
+   if(cur==1) {cur = 2;}
+   else if(cur==2) {cur = 1;}
+   document.getElementById('submitter1').innerHTML= "<p>Question: </p><p>" + cha + "</p>";
+   document.getElementById('submitter2').innerHTML= "<p><input type='text' id='answer'/><input type='submit' onclick='challenge()'/></p>";
    
-   //setting up i to the counting element c at the beginning of each object
-   //so I can keep track of the questions, which were asked
-   i = z[0].c;
-   
-   //setting up the question
-   question = z[i].q;
-   
-   //Error message, if no questions are left
-   if (question == 'end') {
-      alert(noquestions);
-      return;
-    } 
-    
-   //increase the counting element by 1 
-   z[0].c++;
-   
-   //set up the answer
-   answer = z[i].a;
-   
-   //set up the points
-   points = parseFloat(z[i].p);
-   setquestion(question);
 }
 
-//function for keeping track of the teams and the rounds
-function setcounter() {
-   answer = "take";
-   teams_length--;
-   team_count++;
-   if (teams_length == 0) {
-      team_count=0;
-      teams_length=team_length;
+/*
+ The following functions are checking if someone wins
+ */
+function check_r(a) {
+   var c=0;
+   var c2=0;
+   for(var x=0;c<4&&c2<4&&x<7;x++) {
+       if(a[x] == 1){
+          c++;
+          c2=0; 
+       }
+       else if(a[x] == 2) {
+          c=0;
+          c2++; 
+       }
+       else{
+          c=0;
+          c2=0;
+       }
    }
-   
-   
-   round_count--;
-   if(round_count==0) {
-      //tells the players that the game is over
-      alert('The Game is over.');
-      var a = 0;
-      //creates the result page
-      var score = "";
-      for (t in teams) {
-         a++;
-         score = score + "Team/Player" + a + ": " + teams[t] + "; ";
-        }
-        document.getElementById('main').innerHTML = score;
-    }
+   if(c==4) return 1;
+   else if(c2==4) return 2;
+   else return 0;
 }
 
-//function for checking the answers
-function check() {
-    //get the user's answer
-    useranswer = document.getElementById('answer').value;
-    
-    document.getElementById('main').innerHTML="";
-    document.getElementById('main').innerHTML= quiz_game;
-    
-    //Error message, if the player forget to pick a new question.
-    if (answer == 'take') {
-        alert('Please take a new question!')
-        return;
+function check_row() {
+   for(var z=0;z<6;z++) {
+       var win = check_r(arr[z]);
+       if (win != 0) return win;
+   }
+   return 0;
+}
+
+function check_c(z) {
+   var c=0;
+   var c2=0;
+   for(var x=0;c<4&&c2<4&&x<6;x++) {
+       if(arr[x][z] == 1){
+          c++;
+          c2=0; 
+       }
+       else if(arr[x][z] == 2){
+          c=0;
+          c2++; 
+       }
+       else{
+          c=0;
+          c2=0;
+       }
+   }
+   if(c==4) return 1;
+   else if(c2==4) return 2;
+   else return 0;
+}
+
+function check_column() {
+   for(var s=0;s<7;s++) {
+       var win = check_c(s);
+       if (win != 0) return win;
+   }
+   return 0;
+}
+
+function diag1(x,y) {
+    var c=0;
+    var c2=0;
+    var max=0;
+    if (x==2) max=4;
+    else if(x==1) max=5;
+    else if(x==0){
+       if(y==0||y==1) max=6;
+       else if(y==2) max=5;
+       else if(y==3) max=4;
     }
-    
-    //this if close is true, if the answer is right
-    if (answer ==  useranswer) {
-        alert("You are right " + "Your answer: " + useranswer + "; " + " Correct answer: " + answer);
-        teams[team_count] = teams[team_count] + points;
-        setcounter();
-        return;
+    for(var a=0;a<max;a++) {
+        if(arr[x][y]==1) {
+           c++;
+           c2=0; 
+        }
+        else if(arr[x][y]==2) {
+           c=0;
+           c2++; 
+        }
+        else if(arr[x][y]==0) {
+           c=0;
+           c2=0; 
+        }
+        x++;
+        y++;
     }
-    
-    //this if close is true, if the answer is wrong
-    else if (answer != useranswer) {
-        alert("You are wrong! " + "Your answer: " + useranswer + "; " + " Correct answer: " + answer);
-        teams[team_count] = teams[team_count] - points;
-        setcounter();
-        return;
+    if(c==4)return 1;
+    else if(c2==4)return 2;
+    else return 0;
+}
+
+function diag2(x,y) {
+    var c=0;
+    var c2=0;
+    var max=0;
+    if (x==2) max=4;
+    else if(x==1) max=5;
+    else if(x==0){
+       if(y==5||y==6) max=6;
+       else if(y==4) max=5;
+       else if(y==3) max=4;
+    }
+    for(var a=0;a<max;a++) {
+        if(arr[x][y]==1) {
+           c++;
+           c2=0; 
+        }
+        else if(arr[x][y]==2) {
+           c=0;
+           c2++; 
+        }
+        else if(arr[x][y]==0) {
+           c=0;
+           c2=0; 
+        }
+        x++;
+        y--;
+    }
+    if(c==4)return 1;
+    else if(c2==4)return 2;
+    else return 0;
+}
+function check_diag1() {
+    for(var a=0;a<3;a++) {
+       for(var b=0;b<4;b++) {
+          var win = diag1(a,b);
+          if(win!=0)return win;
+       }
+    }
+    return 0;
+}
+function check_diag2() {
+    for(var a=0;a<3;a++) {
+       for(var b=3;b<7;b++) {
+          var win = diag2(a,b);
+          if(win!=0)return win;
+       }
+    }
+    return 0;
+}
+function check_diagonal() {
+   var win = check_diag1();
+   if(win==0){
+      win = check_diag2();
+   }
+   if(win!=0){
+    return win;
+   }
+   return 0;
+}
+
+//check is someone winning
+function check_win() {
+   var win = check_column();
+   if(win==0) win = check_row();
+   if(win==0) win = check_diagonal();
+   if(win!=0) {
+       if (win=='1') { var winpic = "<img height=500px width=900px src='./rsc/images/redwin.jpg'/>"; }
+       if (win=='2') { var winpic = "<img height=500px width=900px src='./rsc/images/yellowwin.jpg/>";}
+       document.getElementById('main').innerHTML= winpic;
+   }
+}
+ //makes the move 
+function move(row) {
+    var check = false;
+    for (var series=5;check == false&&series>=0;series--) {
+       if (arr[series][row] == 0) {
+          var n = "r"+series+row;
+          check = true;
+          if(cur==1) {
+              arr[series][row] = cur; document.getElementById(n).style.background = s1;
+          }
+          else if(cur==2) {
+              arr[series][row] = cur; document.getElementById(n).style.background = s2;
+          }
+          check_win();
+       }
+       else{
+          if(series==0) {
+             alert("The column is full, try another one!");
+          }
+        }
     }
 }
